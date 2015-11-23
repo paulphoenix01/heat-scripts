bf082f6acc183df6678c7f5dfb6a15efb723fadd
Ali Zaringhalam <azaringhalam@juniper.net>
2015-11-23 11:27:31 -0500

    Initial Commit

---
+++ heatclient/create_service_policy.py
@@ -0,0 +1,24 @@
+'''
+Created on Nov 23, 2015
+
+@author: azaringh
+'''
+import create_stack
+
+config_node_ip = '10.10.10.156'
+project_name = 'zeppo'
+
+StackData = {   'stack_name': project_name + '-service-policy',
+                'yaml_file':'../templates/service_policy.yaml',
+                'jinja_path': '../jinja/',
+                'jinja_file': 'service_policy.jinja',
+                'stack_template': { 
+                                    'policy_name': project_name + '-service-policy',
+                                    'service_instance_name': 'default-domain:' + project_name + ':' + project_name + '-service-instance',
+                                    'net_1_name': project_name + '-vnet-1',
+                                    'net_2_name': project_name + '-vnet-2'
+                                    }
+            }
+stack = create_stack.create_stack(config_node_ip, project_name, **StackData)
+ 
+print stack
