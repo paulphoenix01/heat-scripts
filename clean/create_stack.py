0968bccbd56b0905dfd8b197e22f12fce0a47eb9
root <paulphoenix01@gmail.com>
2017-11-25 18:48:40 -0500

    Added clean dir

---
+++ clean/create_stack.py
@@ -0,0 +1,48 @@
+'''
+Version 2: 
+Created on Nov 24, 2017
+@author: azaringh + lphan
+'''
+
+import yaml
+import jinja2
+from heatclient import client
+from keystoneauth1.identity import v2
+from keystoneauth1 import session
+from keystoneauth1 import loading
+
+CONFIG_IP = '192.168.250.1'
+username = 'admin'
+password = 'contrail123'
+auth_url = 'http://' + CONFIG_IP + ':5000/v2.0'
+
+
+def create_stack( tenant_name='demo', **kwargs):
+    
+    #Authenticate
+    loader = loading.get_plugin_loader('password')
+    auth = loader.load_from_options(auth_url=auth_url, username=username,password=password,project_name=tenant_name)
+    sess = session.Session(auth=auth)
+    
+    #Heat Client
+    heatclient = client.Client('1', session=sess)
+
+    #Open Yaml file, with jinja template
+    f = open(kwargs['yaml_file'])
+    txt = f.read()
+    
+    templateLoader = jinja2.FileSystemLoader( searchpath=kwargs['jinja_path'])
+    templateEnv = jinja2.Environment( loader=templateLoader )
+    TEMPLATE_FILE = kwargs['jinja_file']
+    template = templateEnv.get_template( TEMPLATE_FILE )
+    template_vars = kwargs['stack_template']
+    data = yaml.load(template.render( template_vars ))
+    
+    #Assemble all params
+    tx = { "files": {}, "disable_rollback": "true", "stack_name": kwargs['stack_name'], "template": txt, "parameters": data, "environment": {}}
+    
+    #Create Stack
+    stack = heatclient.stacks.create(**tx)
+    
+    return stack
+    
---
+++ clean/create_vm.py
@@ -0,0 +1,55 @@
+'''
+Created on Nov 24, 2017
+
+@author: azaringh
+'''
+import create_stack
+import sys
+from pprint import pprint
+
+user_input = sys.argv
+
+config_node_ip = '192.168.250.1'
+
+if len(user_input) != 5 or user_input[2] =='h':
+        print "create_vm <stack-name> <tenant-name> <vm-name> <subnet x.x.x.x/24>"
+        print "Example: python create_vm.py create-vm1-stack demo demo-vm1 192.168.1.0/24"
+	print "Example python create_vm.py create-vm2-stack demo demo-vm2 192.168.2.0/24"
+        exit(1)
+
+#Parse User_input
+stack_name = user_input[1]
+project_name = user_input[2]
+vm_name = user_input[3]
+network = user_input[4].split('/')
+
+#Network name/subnet
+private_net_name = vm_name + '-vnet'
+private_network_prefix = network[0]
+private_network_prefix_len = network[1]
+
+port_name_1 = vm_name + '-port'
+addr_from_start_true = "True"
+sg_name = vm_name + '-sg'
+
+
+StackData = { 'stack_name': stack_name,                
+               'yaml_file':'../templates/deploy_vm_v3.yaml',
+                'jinja_path': '../jinja/',
+                'jinja_file': 'deploy_vm_v3.jinja',
+                 'stack_template': {
+                                         'private_net_name' : private_net_name,
+                                         'private_network_prefix': private_network_prefix,
+                                         'private_network_prefix_len' : private_network_prefix_len,
+                                         'addr_from_start_true': addr_from_start_true,
+					 'port_name_1': port_name_1,
+					 'sg_name' : sg_name,
+					 'server_name': vm_name,
+                                         'image': 'ubuntu-stress-test',
+                                         'flavor': 'm1.medium'
+                                         }
+                }
+
+#pprint(StackData)
+stack = create_stack.create_stack(project_name, **StackData)
+print stack
