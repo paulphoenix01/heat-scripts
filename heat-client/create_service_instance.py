f7e2d88544be5d3057c2f659fb3b30487e7c7b81
lphan <lphan@juniper.net>
2017-12-21 16:34:16 -0500

    Added services heatclient

--- heat-client/create_service_instance.py
+++ heat-client/create_service_instance.py
@@ -4,23 +4,38 @@ Created on Nov 22, 2015
 @author: azaringh
 '''
 import create_stack
+import sys
 
-config_node_ip = '10.10.10.156'
-project_name = 'zeppo'
+user_input = sys.argv
 
-StackData = { 'stack_name': project_name + '-service-instance',
+
+if len(user_input) != 6 or user_input[2] =='h':
+        print "create_service_template <stack-name> <template-name> <instance_name> <vnet-1-name> <vnet-2-name>"
+	print "Example: python create_service_template.py fw-instance-stack firewall firewall demo-vm1-vnet demo-vm2-vnet"
+	exit(1)
+
+project_name = 'appformix'
+stack_name = user_input[1]
+
+template_name = user_input[2] + '-service-template'
+instance_name = user_input[3] + '-service-instance'
+
+net_1_name = user_input[4]
+net_2_name = user_input[5]
+
+StackData = { 'stack_name': stack_name,
                 'yaml_file':'../templates/service_instance.yaml',
                 'jinja_path': '../jinja/',
                 'jinja_file': 'service_instance.jinja',
                 'stack_template': { 
-                                   'instance_name': project_name + '-service-instance',
-                                   'template': 'default-domain:' + project_name + '-service-template',
-                                   'net_1_name': 'default-domain:' + project_name + ':' + project_name + '-vnet-1',
-                                   'net_2_name': 'default-domain:' + project_name + ':' + project_name + '-vnet-2',
+                                   'instance_name': instance_name,
+                                   'template': 'default-domain:' + template_name,
+                                   'net_1_name': 'default-domain:' + project_name + ':' + net_1_name,
+                                   'net_2_name': 'default-domain:' + project_name + ':' + net_2_name,
                                    'max_instances': 2
                                     }
                }
 
-stack = create_stack.create_stack(config_node_ip, project_name, **StackData)
+#stack = create_stack.create_stack(project_name, **StackData)
  
 print stack
---
+++ heat-client/create_service_policy.py
@@ -0,0 +1,38 @@
+'''
+Created on Nov 23, 2015
+
+@author: azaringh
+'''
+import create_stack
+import sys
+
+user_input = sys.argv
+
+if len(user_input) != 5 or user_input[2] =='h':
+        print "create_service_policy <stack-name> <instance-name> <vnet-1-name> <vnet-2-name>"
+	print "Example: python create_service_policy.py fw-policy-stack firewall-service-instance demo-vm1-vnet demo-vm2-vnet"
+	exit(1)
+
+project_name = 'appformix'
+stack_name = user_input[1]
+
+instance_name = user_input[2]
+policy_name = instance_name + '-policy'
+
+net_1_name = user_input[3]
+net_2_name = user_input[4]
+
+StackData = {   'stack_name': stack_name,
+                'yaml_file':'../templates/service_policy.yaml',
+                'jinja_path': '../jinja/',
+                'jinja_file': 'service_policy.jinja',
+                'stack_template': { 
+                                    'policy_name': policy_name,
+                                    'service_instance_name': 'default-domain:' + project_name + ':' + instance_name,
+                                    'net_1_name': net_1_name,
+                                    'net_2_name': net_2_name
+                                    }
+            }
+stack = create_stack.create_stack(project_name, **StackData)
+ 
+print stack
