f963bfb2088d6e3c79ba54784273d547ad4149b9
lphan <lphan@juniper.net>
2017-12-21 17:01:11 -0500

    Add service to deploy_stack

--- heat-client/deploy-stack.sh
+++ heat-client/deploy-stack.sh
@@ -1,5 +1,3 @@
-#python create_public_net.py public-net-stack public-net 10.1.102.0/24
-#sleep 5
 python create_vm.py create-vm1-stack demo-vm1 192.168.1.0/24
 sleep 5
 python create_vm.py create-vm2-stack demo-vm2 192.168.2.0/24
@@ -8,8 +6,14 @@ sleep 60
 
 python attach_network_policy.py attach-policy-stack demo-vm1-vnet demo-vm2-vnet
 sleep 5
-#python attach_network_policy.py attach-policy-stack1 public-net demo-vm1-vnet
-#sleep 5
-#python attach_network_policy.py attach-policy-stack2 public-net demo-vm2-vnet
-#sleep 5
+
+echo ">> Creating Service template.."
+python create_service_template.py fw-template-stack firewall
+
+echo "\t\t .. Service Instance .. Net1 <> Firewall <> Net2"
+python create_service_template.py fw-instance-stack firewall firewall demo-vm1-vnet demo-vm2-vnet
+
+echo "\t\t .. Service Policy"
+python create_service_policy.py fw-policy-stack firewall-service-instance demo-vm1-vnet demo-vm2-vnet
+
 echo "Done"
