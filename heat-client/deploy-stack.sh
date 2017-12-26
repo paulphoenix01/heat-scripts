e4d6955249ce7c50b9ecec4cab756be0910b3496
lphan <lphan@juniper.net>
2017-12-26 06:07:02 -0500

    Added Appformixed, commented service

--- heat-client/deploy-stack.sh
+++ heat-client/deploy-stack.sh
@@ -7,13 +7,13 @@ sleep 60
 python attach_network_policy.py attach-policy-stack demo-vm1-vnet demo-vm2-vnet
 sleep 5
 
-echo ">> Creating Service template.."
-python create_service_template.py fw-template-stack firewall
+#echo ">> Creating Service template.."
+#python create_service_template.py fw-template-stack firewall
 
-echo "         .. Service Instance .. Net1 <> Firewall <> Net2"
-python create_service_instance.py fw-instance-stack firewall firewall demo-vm1-vnet demo-vm2-vnet
+#echo "         .. Service Instance .. Net1 <> Firewall <> Net2"
+#python create_service_instance.py fw-instance-stack firewall firewall demo-vm1-vnet demo-vm2-vnet
 
-echo "         .. Service Policy"
-python create_service_policy.py fw-policy-stack firewall-service-instance demo-vm1-vnet demo-vm2-vnet
+#echo "         .. Service Policy"
+#python create_service_policy.py fw-policy-stack firewall-service-instance demo-vm1-vnet demo-vm2-vnet
 
 echo "Done"
