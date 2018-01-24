ad9d04983e35aec49993a89d8557008728bba3f0
root <root@nugget-linux.juniper.net>
2018-01-24 09:43:52 -0800

    Working Scale-in Scale-Out

--- salt-sample/event_reactor.py
+++ salt-sample/event_reactor.py
@@ -25,27 +25,26 @@ def run_update_script(max_instance, alerted_name):
 			f.write(str(item) +'\n')
 	os.system(cmd)
 
-latest_event = lines[-1].split(" ")
+latest_events = [lines[-1].split(" "), lines[-2].split(" "), lines[-3].split(" ")]
 
 alerted_instance = instances[1].rstrip()
 max_instance = int(instances[0])
 
-instance_name = latest_event[1]
-metric_value = float(latest_event[-1])
+for latest_event in latest_events:
+	instance_name = latest_event[1]
+	metric_value = float(latest_event[-1])
 
-print "Instance " + instance_name + "\t CPU: " + str(metric_value)
+	#Only check firewall CPU services
+	if 'firewall' in instance_name:
+		print "Alerted: " + alerted_instance + " and instance_name: " + instance_name
+		if metric_value >= 60 and max_instance == 2:
+			#update - scale up to 3
+			run_update_script(3, instance_name)
 
-#Only check firewall CPU services
-if 'firewall' in instance_name:
-	print "Alerted: " + alerted_instance + " and instance_name: " + instance_name
-	if metric_value >= 60 and max_instance == 2:
-		#update - scale up to 3
-		run_update_script(3, instance_name)
-
-	elif metric_value < 40 and max_instance == 3 and alerted_instance == instance_name:
-		#update - scale down to 2
-		msg = "Scaling Down >>> metric: " + str(metric_value) + " max: " + max_instance + "alerted_instance " + alerted_instance
-		run_update_script(2, msg)
-	else:
-		print "CPU: " + str(metric_value) + " and Max_instance: " + str(max_instance) + ", no further action needed"
+		elif metric_value < 40 and max_instance == 3 and alerted_instance == instance_name:
+			#update - scale down to 2
+			msg = instance_name
+			run_update_script(2, msg)
+		else:
+			print "CPU: " + str(metric_value) + " and Max_instance: " + str(max_instance) + ", no further action needed"
 
